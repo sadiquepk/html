64a529eeb14817c8b3ac8c114068849fe604daf7
Sadique <pro.sadiquemuhammed@outlook.com>
2018-07-19 10:35:41 +0530

    added

---
+++ php/pogrm.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$a="sadiq";
+if ($a=="sadiq")
+{
+	# code...
+echo "sadiqs";
+}
+
+?>
+</body>
+</html>
---
+++ php/while loop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$x = 1; 
+
+while($x <= 5) {
+    echo "The number is: $x <br>";
+    $x++;
+    }
+?>
+</body>
+</html>
