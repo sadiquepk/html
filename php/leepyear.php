322b688e1bfb370fa82d7191681fbbea643d3d34
Sadique <pro.sadiquemuhammed@outlook.com>
2018-07-25 10:19:23 +0530

    added

---
+++ php/leepyear.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>lpyear</title>
+</head>
+<body>
+<?php
+$value;
+$value = 2016;
+echo "enter a year <br>";
+echo "$value <br>";
+if ($value % 4 == 0)
+ {
+
+ 	echo "$value is a leap year";
+	# code...
+}
+else
+{
+	echo "$value is not a leap year";
+}
+?>
+</body>
+</html>
--- php/loop array.php
+++ php/loop array.php
@@ -6,7 +6,21 @@
 <body>
 <?php
 $cars = array('bmw','benz','porche');
-echo "$cars";
+$arrlength = count($cars);
+
+for($x = 0; $x < $arrlength; $x++)
+ {
+    echo $cars[$x];
+    echo "<br>";
+}
+
+$age = array('peter' =>35 ,'joe'=>44,'mak'=>50);
+foreach($age as $x => $x_value)
+ {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
+
 ?>
 </body>
 </html>
