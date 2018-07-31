412bc8690a069530a981a423666b88063a9cfe85
Sadique <pro.sadiquemuhammed@outlook.com>
2018-07-31 10:55:57 +0530

    added

--- php/sql.php
+++ php/sql.php
@@ -1,41 +1,37 @@
-
 <?php
 $servername="localhost";
 $username="root";
 $password="";
-$dbname="dbs";
-
-$conn=new mysqli($servername,$username,$password);
-
-
+$dbname="DBS";
+$conn=new mysqli($servername,$username,$password,$dbname);
 if ($conn->connect_error)
  {
-
  	echo "connection fail";
 	# code...
-}
-
+ }
 else
 {
 	echo "connection success";
 }
-
 $sql = "CREATE DATABASE DBS";
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE) 
+{
     echo "Database created successfully";
-} else {
-    echo "Error creating database: ";
 }
-$sql = "insert into my_guest(firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
+ else 
+ {
+    echo "Error creating database: ";
+ }
+
+$sql ="INSERT INTO user_info(first_name,last_name,email_id) VALUES ('john', 'Doe','john@example.com')";
+$sql="INSERT into user_info(first_name,last_name,email_id) VALUES('tim','cook','timcook@example.com')";
 
 if ($conn->query($sql) === TRUE)
  {
     echo "New record created successfully";
-} 
-else
+ }
+ else
  {
-    echo "Error";
-}
-
+    echo "Error: " . $sql . "<br>" . $conn->error;
+ }
 ?>
