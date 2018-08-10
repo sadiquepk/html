ed99e275a0fc9a73f8bcde3555c7c4537bbb77e7
Sadique <pro.sadiquemuhammed@outlook.com>
2018-08-10 10:48:50 +0530

    added

--- php/phpform.php
+++ php/phpform.php
@@ -4,11 +4,28 @@
 	<title></title>
 </head>
 <body>
+	<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="dbs";
+$mysqli=new mysqli($servername,$username,$password,$dbname);
+
+$sql="SELECT `cv_id`, `cv_Fname`, `cv_Lname`, `cv_Eid` FROM `tbl_cv` WHERE `cv_id` =11 ";
+
+$res=mysqli_query($mysqli,$sql);
+
+$row=mysqli_fetch_assoc($res);
+//print_r($row);
+?>
+
 <form action="sql11.php" method="post">
-	<input type="text" name="cv_Fname" placeholder="firstname">
-	<input type="text" name="cv_Lname" placeholder="lastname">
-	<input type="text" name="cv_Eid" placeholder="emailid">
+    <input type="text" name="cv_id" placeholder="cv_id" value="<?=$row['cv_id']?>">                                               
+	<input type="text" name="cv_Fname" placeholder="firstname" value="<?=$row['cv_Fname']?>">
+	<input type="text" name="cv_Lname" placeholder="lastname" value="<?=$row['cv_Lname']?>">
+	<input type="text" name="cv_Eid" placeholder="emailid" value="<?=$row['cv_Eid']?>">
     <input type="submit" name="">
 </form>
+
 </body>
 </html>
--- php/sql11.php
+++ php/sql11.php
@@ -4,7 +4,16 @@ $username="root";
 $password="";
 $dbname="dbs";
 $conkey=new mysqli($servername,$username,$password,$dbname);
-$sql="UPDATE `tbl_cv` SET `cv_Fname` = 'zaadi', `cv_Lname` = 'daa', `cv_Eid` = 'nas' WHERE `tbl_cv`.`cv_id` = 6";
+//print_r($_POST);
+
+$id=$_POST['cv_id'];
+$fname=$_POST['cv_Fname'];
+$lname=$_POST['cv_Lname'];
+$eid=$_POST['cv_Eid'];
+
+$sql="UPDATE `tbl_cv` SET `cv_Fname` ='$fname', `cv_Lname` = '$lname', `cv_Eid` = '$eid' WHERE `tbl_cv`.`cv_id` =11";
+
+
 if(mysqli_query($conkey,$sql))
 {
 	echo "updated";
