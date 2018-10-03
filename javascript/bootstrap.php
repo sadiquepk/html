138ee13de86669048f2e622af815c317ab04559f
sadiquepk <ksadiquepktr@gmail.com>
2018-10-03 18:16:20 +0530

    3-10-18

---
+++ javascript/bootstrap.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MY BOOTSTRAP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+    <div class ="container-fluid">
+      
+        <nav class="navbar  navbar-dark bg-dark  navbar-expand-lg fixed-top">
+            
+            <h2 class="navbar-brand">Start Bootstrap</h2>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+         <div id="menu" class="collapse navbar-collapse ">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a href ="#" class="nav-link text-light">SAMPLE POST</a></li>
+                <li class="nav-item"><a href ="#" class="nav-link text-light">HOME</a></li>
+                <li class="nav-item"><a href ="#" class="nav-link text-light">ABOUT</a></li>
+                <li class="nav-item"><a href ="#" class="nav-link text-light">CONTACT</a></li>
+                <li class="nav-item"><a href="#"  class="nav-link text-light">SEARCH</a></li>
+                
+           </ul> 
+           </div>   
+  </nav>
+ <div style="background-image:url('qq.jpg');background-size:cover;" class="text-center py-5">
+ <p class="py-5 my-5  text-primary  display-1 " >MY BLOG IS START</p></div> 
+ 
+</div>
+<div class="container  mt-5 pt-5">
+    <div class="row">
+     <div class="col-lg-6 border text-center border-dark"><h1>Type here to search</h1></div>
+     <div class="col-lg-6 border border-danger"><h1>Type here to search</h1></div>
+
+
+    </div>
+
+
+
+
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
