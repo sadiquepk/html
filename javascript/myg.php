35b83a91b5309cfa4a86b808eeb683d99b693fa9
sadiquepk <ksadiquepktr@gmail.com>
2018-10-10 16:23:46 +0530

    added successfully

--- javascript/myg.php
+++ javascript/myg.php
@@ -16,7 +16,7 @@
 <body>
 <div class ="container">
      <nav class="navbar  navbar-dark bg-dark  navbar-expand-lg fixed-top ">
-     <h2 class="navbar-brand px-5">STARTBOOTSTRAP</h2>
+     <h4 class="navbar-brand px-5">STARTBOOTSTRAP</h4>
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
      <span class="navbar-toggler-icon"></span>
      </button>
@@ -38,7 +38,26 @@
      <div class="col-lg-6 "><h1 class="display-4 font-weight-bold">For those about to rock...</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p></div>
      <div class="col-lg-6 "><center><img src="image/img01.jpg" alt="" class="rounded-circle w-75"></center></div>
     </div>
+    <div class="row">
+    
+     <div class="col-lg-6 pt-5 py-5"><center><img src="image/img02.jpg" alt="" class="rounded-circle w-75"></center></div>
+     <div class="col-lg-6 pt-5 "><h1 class="display-4 font-weight-bold">We salute you!</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p></div>
+    </div>
+    <div class="row">
+     <div class="col-lg-6 "><h1 class="display-4 font-weight-bold">Let there be rock!</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p></div>
+     <div class="col-lg-6 "><center><img src="image/img03.jpg" alt="" class="rounded-circle w-75"></center></div>
+    </div>
+    
 </div>
+<div class="card-footer bg-dark text-light border-dark pt-5 mt-5 text-center">Copyright © Your Website 2018
+    </div>
+</div>
+
+
+
+
+
+
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>   
---
+++ javascript/star.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<style>
+ a:hover{
+    color:red;
+}
+h4{
+    font-family:"Comic Sans MS", cursive, sans-serif;
+}
+</style>
+<body>
+<div class="container-fluid">
+<nav class="navbar  navbar-dark bg-dark  navbar-expand-lg fixed-top ">
+     <h4 class="navbar-brand px-5 text-uppercase">tipu exports</h4>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
+     <span class="navbar-toggler-icon "></span>
+     </button>
+    <div id="menu" class="collapse navbar-collapse ">
+    <ul class="navbar-nav ml-auto">
+<li class="nav-item"><a href="#" class="nav-link text-light">HOME</a></li>
+<li class="nav-item"><a href="#" class="nav-link text-light">SERVICE</a></li>
+<li class="nav-item"><a href="#"class="nav-link text-light">ABOUT US</a></li>
+<li class="nav-item"><a href="#"class="nav-link text-light">MISSION&VISION</a></li>
+<li class="nav-item"><a href="#" class="nav-link text-light">CONTACT</a></li>
+</ul>
+</div>
+</nav>
+</div>
+<div style="background-image:url('image/homebg.jpg');background-size:cover;" class="text-center py-5 border danger container-fluid">
+<p class="pt-5 mt-5  text-primary  display-2 font-weight-bold text-uppercase" >tipu exports</p><p class="display-4 text-primary font-weight-bold text-uppercase">tipuexports.com</p><button class="btn btn-primary btn-xl rounded-pill py-3 px-5 mb-5 mt-3"id="btn">LearnMore</button></div> 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> 
+</body>
+</html>
